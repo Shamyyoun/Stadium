a9c746269a5365708dfe9a022101ecee4af0c387
karam <moshkaram2@gmail.com>
2016-06-30 20:47:39 +0200

    Add My team list screen

---
+++ app/src/main/java/com/stadium/player/models/MyTeamsModel.java
@@ -0,0 +1,42 @@
+package com.stadium.player.models;
+
+/**
+ * Created by karam on 6/30/16.
+ */
+public class MyTeamsModel  {
+
+        private String teamName;
+        private String teamImage;
+        private String teamRule;
+
+        public static final String TeamName = "";
+        public static final String TeamImage = "";
+        public static final String TeamRule = "";
+
+
+        public String getTeamName() {
+            return teamName;
+        }
+
+        public void setTeamName(String teamName) {
+            this.teamName = teamName;
+        }
+
+        public String getTeamImage() {
+            return teamImage;
+        }
+
+        public void setTeamImage(String teamImage) {
+            this.teamImage = teamImage;
+        }
+
+        public String getTeamRule() {
+        return teamRule;
+        }
+
+        public void setTeamRule(String teamRule) {
+        this.teamRule = teamRule;
+        }
+}
+
+
