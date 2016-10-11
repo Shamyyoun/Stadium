4a838b6ce1dc3106bf13df4bffee702be6e37c04
Mahmoud Elshamy <shamyyoun@gmail.com>
2016-10-11 21:29:50 +0300

    ParentActivity and ParentToolbarActivity have been merged to ParentActivity

--- app/src/main/java/com/stadium/app/activities/ParentActivity.java
+++ app/src/main/java/com/stadium/app/activities/ParentActivity.java
@@ -5,8 +5,12 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.stadium.app.R;
 import com.stadium.app.connection.ConnectionHandler;
@@ -23,6 +27,12 @@ public class ParentActivity extends AppCompatActivity implements View.OnClickLis
     protected AppCompatActivity activity;
     private FrameLayout rootView;
     protected ProgressDialog progressDialog;
+    private Toolbar toolbar;
+    private TextView tvToolbarTitle;
+    private int menuId;
+    private boolean enableBack;
+    private int iconResId;
+    private String toolbarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,32 @@ public class ParentActivity extends AppCompatActivity implements View.OnClickLis
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         activity = this;
         rootView = (FrameLayout) findViewById(android.R.id.content);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // init the view_toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // check the view_toolbar
+        if (toolbar != null) {
+            // view_toolbar is available >> handle it
+            setSupportActionBar(toolbar);
+            toolbar.setTitle("");
+            tvToolbarTitle = (TextView) toolbar.findViewById(R.id.tv_toolbar_title);
+            tvToolbarTitle.setText(getTitle());
+
+            // check if enable back
+            if (enableBack) {
+                // set the back icon
+                toolbar.setNavigationIcon(R.drawable.back_icon);
+            } else if (iconResId != 0) {
+                // set this icon
+                toolbar.setNavigationIcon(iconResId);
+            }
+        }
     }
 
     public void logE(String msg) {
@@ -90,6 +126,71 @@ public class ParentActivity extends AppCompatActivity implements View.OnClickLis
     public void hideKeyboard() {
         if (rootView != null) {
             Utils.hideKeyboard(rootView);
+        }
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+        if (tvToolbarTitle != null) {
+            tvToolbarTitle.setText(title);
+        }
+    }
+
+    @Override
+    public void setTitle(int titleId) {
+        toolbarTitle = getString(titleId);
+        if (tvToolbarTitle != null) {
+            tvToolbarTitle.setText(titleId);
+        }
+    }
+
+    public void hideToolbarTitle() {
+        if (tvToolbarTitle != null) {
+            tvToolbarTitle.setVisibility(View.GONE);
+        }
+    }
+
+    public void showToolbarTitle() {
+        if (tvToolbarTitle != null) {
+            tvToolbarTitle.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void createOptionsMenu(int menuId) {
+        this.menuId = menuId;
+        invalidateOptionsMenu();
+    }
+
+    public void removeOptionsMenu() {
+        menuId = 0;
+        invalidateOptionsMenu();
+    }
+
+    public void enableBackButton() {
+        enableBack = true;
+    }
+
+    public void setToolbarIcon(int iconResId) {
+        this.iconResId = iconResId;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (menuId != 0) {
+            getMenuInflater().inflate(menuId, menu);
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home && enableBack) {
+            onBackPressed();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
     }
 }
