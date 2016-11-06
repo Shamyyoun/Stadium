64a4fb1339069c88d8811db4c70f73435088f3eb
Mahmoud Elshamy <shamyyoun@gmail.com>
2016-11-06 02:01:27 +0200

    Order players is done.

--- app/src/main/java/com/stadium/app/dialogs/OrderPlayersDialog.java
+++ app/src/main/java/com/stadium/app/dialogs/OrderPlayersDialog.java
@@ -1,17 +1,101 @@
 package com.stadium.app.dialogs;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.stadium.app.R;
+import com.stadium.app.adapters.RadioButtonsAdapter;
+import com.stadium.app.controllers.OrderController;
+import com.stadium.app.interfaces.OnCheckableSelectedListener;
+import com.stadium.app.models.entities.OrderCriteria;
+
+import java.util.List;
 
 /**
  * Created by Shamyyoun on 6/28/16.
  */
 public class OrderPlayersDialog extends ParentDialog {
+    private OrderController orderController;
+    private RecyclerView recyclerView;
+    private Button btnSubmit;
+    private RadioButtonsAdapter adapter;
+    private List<OrderCriteria> data;
+    private OnCheckableSelectedListener itemSelectedListener;
+    private int selectedItemType;
 
     public OrderPlayersDialog(final Context context) {
         super(context);
         setContentView(R.layout.dialog_order_players);
         setTitle(R.string.order_by);
+
+        // create controllers
+        orderController = new OrderController();
+
+        // init views
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        btnSubmit = (Button) findViewById(R.id.btn_submit);
+
+        // customize the recycler view
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // add listeners
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSubmit();
+            }
+        });
+
+        // get data and update the ui
+        data = orderController.getPlayersCriterias(context);
+        updateUI();
+    }
+
+    private void updateUI() {
+        adapter = new RadioButtonsAdapter(context, data, R.layout.item_radio_button);
+        recyclerView.setAdapter(adapter);
+
+        // select an item if possible
+        selectCheckedItem();
+    }
+
+    private void onSubmit() {
+        if (adapter != null && data != null && itemSelectedListener != null) {
+            itemSelectedListener.onCheckableSelected(data.get(adapter.getSelectedItemPosition()));
+        }
+
+        dismiss();
+    }
+
+    public void setOnItemSelectedListener(OnCheckableSelectedListener itemSelectedListener) {
+        this.itemSelectedListener = itemSelectedListener;
+    }
+
+    public void setSelectedItemType(int selectedItemType) {
+        this.selectedItemType = selectedItemType;
+    }
+
+    private void selectCheckedItem() {
+        int itemPosition = orderController.getItemPosition(data, selectedItemType);
+        if (itemPosition != -1) {
+            adapter.setSelectedItem(itemPosition);
+        }
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        if (data != null && adapter != null) {
+            selectCheckedItem();
+        }
+    }
+
+    public OrderCriteria getDefaultCriteria() {
+        return data.get(0);
     }
 }
--- app/src/main/java/com/stadium/app/fragments/PlayersFragment.java
+++ app/src/main/java/com/stadium/app/fragments/PlayersFragment.java
@@ -19,8 +19,13 @@ import com.stadium.app.activities.ContactsActivity;
 import com.stadium.app.activities.PlayersSearchActivity;
 import com.stadium.app.adapters.PlayersAdapter;
 import com.stadium.app.connection.ConnectionHandler;
+import com.stadium.app.controllers.OrderController;
 import com.stadium.app.controllers.SearchController;
+import com.stadium.app.dialogs.OrderPlayersDialog;
+import com.stadium.app.interfaces.OnCheckableSelectedListener;
+import com.stadium.app.models.Checkable;
 import com.stadium.app.models.SerializableListWrapper;
+import com.stadium.app.models.entities.OrderCriteria;
 import com.stadium.app.models.entities.PlayersFilter;
 import com.stadium.app.models.entities.User;
 import com.stadium.app.utils.Utils;
@@ -34,13 +39,17 @@ import java.util.List;
  */
 public class PlayersFragment extends ProgressFragment {
     private SearchController searchController;
+    private OrderController orderController;
     private TextView tvOrderBy;
     private RecyclerView recyclerView;
     private List<User> data;
     private PlayersAdapter adapter;
 
+    private List<User> searchResults;
     private boolean enableControls = true;
     private PlayersFilter filter;
+    private OrderPlayersDialog orderDialog;
+    private OrderCriteria orderCriteria;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,6 +64,7 @@ public class PlayersFragment extends ProgressFragment {
 
         // create controllers
         searchController = new SearchController();
+        orderController = new OrderController();
 
         // init views
         tvOrderBy = (TextView) findViewById(R.id.tv_order_by);
@@ -63,6 +73,11 @@ public class PlayersFragment extends ProgressFragment {
         // customize the recycler view
         LinearLayoutManager layoutManager = new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
+
+        // set default order as the data is returned ordered from the server
+        orderDialog = new OrderPlayersDialog(activity);
+        orderCriteria = orderDialog.getDefaultCriteria();
+        updateOrderByUI();
 
         // get data from saved bundle if exists
         if (savedInstanceState != null) {
@@ -87,7 +102,7 @@ public class PlayersFragment extends ProgressFragment {
         tvOrderBy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
+                showOrderDialog();
             }
         });
 
@@ -120,6 +135,18 @@ public class PlayersFragment extends ProgressFragment {
         showMain();
     }
 
+    private void updateOrderByUI() {
+        String orderStr = getString(R.string.the_order_by) + " " + orderCriteria.getName();
+        tvOrderBy.setText(orderStr);
+    }
+
+    private void resetFilters() {
+        filter = null;
+        searchResults = null;
+        orderCriteria = orderDialog.getDefaultCriteria();
+        updateOrderByUI();
+    }
+
     private void loadData() {
         // check internet connection
         if (!Utils.hasConnection(activity)) {
@@ -128,7 +155,9 @@ public class PlayersFragment extends ProgressFragment {
             return;
         }
 
+        // show progress and reset filters
         showProgress();
+        resetFilters();
 
         // send request
         ConnectionHandler connectionHandler = ApiRequests.allPlayers(activity, this);
@@ -136,7 +165,6 @@ public class PlayersFragment extends ProgressFragment {
     }
 
     private void refresh() {
-        filter = null;
         loadData();
     }
 
@@ -170,10 +198,12 @@ public class PlayersFragment extends ProgressFragment {
     }
 
     private void enableControls() {
+        tvOrderBy.setEnabled(true);
         enableControls = true;
     }
 
     private void disableControls() {
+        tvOrderBy.setEnabled(false);
         enableControls = false;
     }
 
@@ -216,8 +246,6 @@ public class PlayersFragment extends ProgressFragment {
 
     private void search() {
         new AsyncTask() {
-            List<User> playersResult;
-
             @Override
             protected void onPreExecute() {
                 showProgressDialog();
@@ -225,20 +253,57 @@ public class PlayersFragment extends ProgressFragment {
 
             @Override
             protected Object doInBackground(Object[] params) {
-                playersResult = searchController.searchPlayers(data, filter);
+                searchResults = searchController.searchPlayers(data, filter);
                 return null;
             }
 
             @Override
             protected void onPostExecute(Object o) {
                 hideProgressDialog();
-                updateUI(playersResult);
+                updateUI(searchResults);
 
                 // check the results size
-                if (playersResult.size() == 0) {
+                if (searchResults.size() == 0) {
+                    // results is empty, show empty view
                     showEmpty(R.string.no_search_results);
+                } else if (orderCriteria != null) { // check if an order exists
+                    // an order exits, order data
+                    order();
                 }
-            }
+             }
         }.execute();
+    }
+
+    private void showOrderDialog() {
+        // set the order type
+        orderDialog.setSelectedItemType(orderCriteria.getType());
+
+        // set listener
+        orderDialog.setOnItemSelectedListener(new OnCheckableSelectedListener() {
+            @Override
+            public void onCheckableSelected(Checkable item) {
+                orderCriteria = (OrderCriteria) item;
+                order();
+                Utils.showShortToast(activity, R.string.ordered);
+            }
+        });
+
+        // show the dialog
+        orderDialog.show();
+    }
+
+    private void order() {
+        // check if has a visible search
+        if (searchResults != null) {
+            // order search results
+            orderController.orderPlayers(searchResults, orderCriteria.getType());
+        } else {
+            // order original data
+            orderController.orderPlayers(data, orderCriteria.getType());
+        }
+
+        // update the ui
+        adapter.notifyDataSetChanged();
+        updateOrderByUI();
     }
 }
